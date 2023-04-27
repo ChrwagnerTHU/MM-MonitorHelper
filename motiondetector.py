45d36cf99f8cf7b8f0591b3e5f74dd5132af3ae3
ChrwagnerTHU <wagnch02@thu.de>
2023-04-27 23:15:47 +0200

    Change to MMM-RemoteControl only

--- motiondetector.py
+++ motiondetector.py
@@ -13,16 +13,15 @@ GPIO.setup(PIR_PIN, GPIO.IN)
 
 url_monitor_dim = 'http://192.168.2.46:8080/remote?action=BRIGHTNESS&value=1'
 url_monitor_bright = 'http://192.168.2.46:8080/remote?action=BRIGHTNESS&value=100'
+url_monitor_on = 'http://192.168.2.46:8080/remote?action=MONITORON'
+url_monitor_off = 'http://192.168.2.46:8080/remote?action=MONITOROFF'
 time_monitor_off = datetime.time(23, 55, 0)
 time_monitor_on = datetime.time(7, 0, 0)
-bash_monitor_on = 'vcgencmd display_power 1'
-bash_monitor_off = 'vcgencmd display_power 0'
 
 
 # Initially turn off monitor
 requests.get(url_monitor_dim)
-process = subprocess.Popen(bash_monitor_on.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
+requests.get(url_monitor_on)
 
 while(time_in_range(time_monitor_on, time_monitor_off, datetime.datetime.now().time())):
     if GPIO.input(PIR_PIN):
@@ -30,5 +29,4 @@ while(time_in_range(time_monitor_on, time_monitor_off, datetime.datetime.now().t
         time.sleep(10)
         requests.get(url_monitor_dim)
 
-process = subprocess.Popen(bash_monitor_off.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
+requests.get(url_monitor_off)
