84e1df2d3c8eff10210cd380566e82bc0d60580e
ChrwagnerTHU <wagnch02@thu.de>
2023-04-29 22:24:09 +0200

    Revert test

--- motiondetector.py
+++ motiondetector.py
@@ -26,9 +26,6 @@ with open (__location__ + "/config.json", "r") as f:
     off_interval = data['MOTION']['off_interval']
 
 
-monitor_on = False
-
-
 # Set GPIO pin for PIR sensor
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
