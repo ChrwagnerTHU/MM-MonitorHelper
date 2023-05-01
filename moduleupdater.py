d70ff7f534374da50b4a006a2df6e3519816f619
ChrwagnerTHU <wagnch02@thu.de>
2023-05-01 21:35:49 +0200

    Add Script to update module updater

--- moduleupdater.py
+++ moduleupdater.py
@@ -1,9 +1,7 @@
 import json
 import os
-import re
+
 import requests
-from bs4 import BeautifulSoup
-import datetime
 
 url = 'https://www.flashscore.de/fussball/deutschland/'
 todayStr = 'Heutige Spiele'
@@ -12,10 +10,16 @@ __location__ = os.path.dirname(os.path.abspath(__file__))
 with open (__location__ + "/config.json", "r") as f:
     data = json.load(f)
     league = data['MODULE']['league']
+    url_league_off = data['MODULE']['url_league_off']
+    url_league_on = data['MODULE']['url_league_on']
+
+# Initially show soccer module
+requests.get(url_league_on)
 
 for l in league:
     response = requests.get(url + l)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    if (soup.get_text().find(todayStr) > 0):
-        print("Hamwa")
-        # Zeige Modul für Bundesliga an
+    if todayStr in response.text:
+        requests.get(url_league_on)
+        break
+    else:
+        requests.get(url_league_off)
